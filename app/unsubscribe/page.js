5b18068933fac2851d965b00545ba8b120894e3e
juniorGek <kossihyacinthe@gmail.com>
2024-11-10 00:45:24 +0000

    useSearch

--- app/unsubscribe/page.js
+++ app/unsubscribe/page.js
@@ -1,40 +1,37 @@
 "use client";
 
 import { postUnSubscribe } from '@/helpers/backend_helper';
-import { message } from 'antd';
+import { message as antdMessage } from 'antd'; // Renomme `message` de antd
 import Link from 'next/link';
 import { useSearchParams } from 'next/navigation';
 import React, { useEffect, useState } from 'react';
 
 const UnSubscribePage = () => {
-    const searchParam = useSearchParams()
-    const email = searchParam.get('email')
-    const [message, setMessage] = useState('')
+    const searchParam = useSearchParams();
+    const email = searchParam.get('email');
+    const [responseMessage, setResponseMessage] = useState(''); // Renomme `message` en `responseMessage`
 
     useEffect(() => {
         if (email) {
             postUnSubscribe({ email })
                 .then((res) => {
-                    console.log('res', res)
-                    setMessage(res?.msg)
+                    console.log('res', res);
+                    setResponseMessage(res?.msg);
                 })
                 .catch((err) => {
-                    setMessage(res?.msg)
-                })
-
+                    setResponseMessage(err?.msg || 'An error occurred');
+                });
         }
-    }, [email])
-
-
+    }, [email]);
 
     return (
-        <div className='flex flex-col h-screen w-screen justify-center items-center ' >
-            {/* <Image src='/404.svg' width={400} height={400} /> */}
-            <h2 className='header_2' >
-                {message}
+        <div className='flex flex-col h-screen w-screen justify-center items-center'>
+            <h2 className='header_2'>
+                {responseMessage}
             </h2>
             <Link className='header_4 text-Primary_Color mt-4' href="/">
-                Return Home</Link>
+                Return Home
+            </Link>
         </div>
     );
 };
